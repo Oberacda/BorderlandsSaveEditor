9885d8f2a99241076b8f007d0c9568cc75124609
David Oberacker <david.oberacker@student.kit.edu>
2019-07-16 23:08:07 +0200

    Fixed building/deployment on windows.

--- include/library.hpp
+++ include/library.hpp
@@ -2,10 +2,10 @@
 #pragma once
 #include <rapidjson/document.h>
 
-#include "bl_save_editor_exports.hpp"
+#include "bl2_save_editor_exports.hpp"
 
-void BORDERLANDS_SAVE_EDITOR_API print(rapidjson::Document &json);
+void BORDERLANDS2_SAVE_EDITOR_API print(rapidjson::Document &json);
 
-rapidjson::Document BORDERLANDS_SAVE_EDITOR_API getDocumentFromPath(const std::string &path);
+rapidjson::Document BORDERLANDS2_SAVE_EDITOR_API getDocumentFromPath(const std::string &path);
 
-void BORDERLANDS_SAVE_EDITOR_API printDocumentToPath(const std::string &path, rapidjson::Document &json);
+void BORDERLANDS2_SAVE_EDITOR_API printDocumentToPath(const std::string &path, rapidjson::Document &json);
